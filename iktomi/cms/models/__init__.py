50a04b104291a5c034b541550b0d9e9c8797e73e
Denis Otkidach <denis.otkidach@gmail.com>
2013-06-18 17:09:22 +0400

    Meaningless comments are removed

--- iktomi/cms/models/__init__.py
+++ iktomi/cms/models/__init__.py
@@ -3,8 +3,5 @@ from .base import metadata, AdminModel
 # For internal use in admin interface
 from internal import *
 
-#from news import *
-#from photos import *
-
 __all__ = [name for name, value in globals().items()
            if isinstance(value, type) and issubclass(value, AdminModel)]
