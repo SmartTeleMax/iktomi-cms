fd5dcaa3ee6d60bb4be8641af0181282c811df64
Denis Otkidach <denis.otkidach@gmail.com>
2013-06-20 19:09:41 +0400

    Models are unique for each project

--- iktomi/cms/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base import metadata, AdminModel
-
-# For internal use in admin interface
-from internal import *
-
-__all__ = [name for name, value in globals().items()
-           if isinstance(value, type) and issubclass(value, AdminModel)]
