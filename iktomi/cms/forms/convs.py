9b8fbdc3d0972656faa9775319df33bbbb7e5fc1
Harut <yes@harutune.name>
2013-09-18 13:04:36 +0400

    DBUnique model argument is now optional

--- iktomi/cms/forms/convs.py
+++ iktomi/cms/forms/convs.py
@@ -2,16 +2,17 @@ from iktomi.unstable.forms.convs import *
 from sqlalchemy.orm import Query
 
 
-def DBUnique(model, message=u'An object with such value already exists'):
+def DBUnique(model=None, message=u'An object with such value already exists'):
 
     @validator(message)
     def to_python(conv, value):
+        model_ = model or conv.field.form.model
         if value not in ('', None):
-            field = getattr(model, conv.field.name)
+            field = getattr(model_, conv.field.name)
             # XXX Using db.query() won't work properly with custom
             # auto-filtering query classes. But silently replacing the class is
             # not good too.
-            query = Query(model, session=conv.env.db)
+            query = Query(model_, session=conv.env.db)
             item = query.filter(field==value).scalar()
             if item is not None and item != conv.field.form.item:
                 return False
