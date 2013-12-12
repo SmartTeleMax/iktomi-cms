61ca89b2adb7d59911f2516e6536f53786f6d6d1
Harut <yes@harutune.name>
2013-12-12 18:17:32 +0400

    cleaned up commented code

--- iktomi/cms/publishing/i18n_loner.py
+++ iktomi/cms/publishing/i18n_loner.py
@@ -1,72 +1,7 @@
 # -*- coding: utf-8 -*-
 from iktomi import web
-#from iktomi.cms.stream_handlers import PrepareItemHandler
 from iktomi.cms.loner import Loner
 from iktomi.cms.publishing.loner import PublishLoner
-from iktomi.utils import cached_property
-#from iktomi.unstable.db.sqla.replication import replicate_attributes
-#from iktomi.cms.publishing.stream import PublishItemHandler, \
-#        PublishStreamNoState, PublishStream
-
-
-#class PrepareI18nItemHandler(PrepareItemHandler):
-#
-#    def __call__(self, env, data):
-#        # XXX Dirty hack to support object creation
-#        env.absent_items = True
-#        return PrepareItemHandler.__call__(self, env, data)
-
-
-#class I18nItemHandler(PublishItemHandler):
-#
-#    # XXX turn off autosave or make it save to DraftForm only?
-#
-#    #PrepareItemHandler = PrepareI18nItemHandler
-#
-#    def get_item_form(self, stream, env, item, initial, draft=None):
-#        if item.state not in (item.ABSENT, item.DELETED):
-#            return PublishItemHandler.get_item_form(
-#                    self, stream, env, item, initial, draft)
-#
-#        # XXX this method looks hacky
-#        # Get existing language version and fill the form with object reflection
-#        # to current language model
-#        for lang in item.models.langs:
-#            # XXX item.models is not an interface
-#            if lang == item.models.lang:
-#                continue
-#            source_item = item._item_version('admin', lang)
-#            if source_item and \
-#                    source_item.state not in (item.ABSENT, item.DELETED):
-#                break
-#        else:
-#            # The item has been deleted on all language versions, creation is
-#            # not allowed
-#            raise HTTPNotFound
-#        # make object reflection, do not add it to db
-#        #fake_item = item.__class__()
-#        ## XXX do not replicate text fields, creation time, etc
-#        #replicate_attributes(source_item, fake_item)
-#        #form = PublishItemHandler.get_item_form(
-#        #        self, stream, env, fake_item, initial, draft)
-#        # XXX hack!
-#        #form.item = item
-#
-#        # hack do get initial value for form from source item
-#        source_form = PublishItemHandler.get_item_form(
-#                    self, stream, env, source_item, initial, draft)
-#        form = PublishItemHandler.get_item_form(
-#                self, stream, env, item, initial, draft)
-#        form.accept(source_form.raw_data)
-#        form.errors = {}
-#
-#        return form
-
-#    def process_item_template_data(self, env, td):
-#        item = td['item']
-#        if item.state in (item.ABSENT, item.DELETED):
-#            td['title'] = u'Создание языковой версии объекта'
-#        return PublishItemHandler.process_item_template_data(self, env, td)
 
 
 class I18nLonerMixin(object):
@@ -114,8 +49,5 @@ class I18nLonerMixin(object):
 class I18nPublishLoner(I18nLonerMixin, PublishLoner):
 
     pass
-#    core_actions = [x for x in PublishStream.core_actions 
-#                    if x.action != 'item'] + [
-#        I18nItemHandler(),
-#    ]
+
 
