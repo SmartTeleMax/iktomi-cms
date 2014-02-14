0fcf7a415358c83701d669389dc75844470afe23
Harut <yes@harutune.name>
2014-02-14 18:16:19 +0400

    mage ordering field name configurable in stream_sortables

--- iktomi/cms/stream_sortables.py
+++ iktomi/cms/stream_sortables.py
@@ -20,61 +20,38 @@ class ListItemModelChoice(convs.ModelChoice):
 
 
 class ListItemForm(Form):
+
+    ordering_field = 'order'
+
+    def __init__(self, *args, **kwargs):
+        self.ordering_field = kwargs.pop('ordering_field', self.ordering_field)
+        Form.__init__(self, *args, **kwargs)
+
     fields = [
-        FieldList(
-            'items', 
-            widget=FieldList.widget(
-                template='list_item_form'),
-            field=FieldSet(
-                'item', 
-                fields=[
-                    Field('item',
-                          conv=ListItemModelChoice(required=False),
-                          label=u'item'),
-                    Field('order',
-                          conv=convs.Int(),
-                          label=u'Порядок'),
-                ]
+            FieldList(
+                'items', 
+                widget=FieldList.widget(
+                    template='list_item_form'),
+                field=FieldSet(
+                    'item', 
+                    fields=[
+                        Field('item',
+                              conv=ListItemModelChoice(required=False),
+                              label=u'item'),
+                        Field("order",
+                              conv=convs.Int(),
+                              label=u'Порядок'),
+                    ]
+                ),
             ),
-        ),
-    ]
+        ]
 
     @classmethod
     def for_items(cls, env, items):
-        initial = {'items': [{'item': item, 'order': item.order}
+        initial = {'items': [{'item': item, 
+                              "order": getattr(item, cls.ordering_field)}
                              for item in items]}
         return cls(env, initial=initial)
-
-
-#def getGroupedListItemForm(Model, init_js='stream_sortable_grouped_init.js'):
-#    # FieldSet('group', ...) added to avoid custom stream row template
-#    class GroupedListItemForm(Form):
-#    
-#        fields = [
-#            FieldList(
-#                'groups',
-#                field=FieldSet(
-#                    'group',
-#                    fields=[
-#                        FieldList(
-#                            'items', 
-#                            field=FieldSet(
-#                                'item', 
-#                                fields=[
-#                                    Field('item', conv=convs.ModelChoice(model=Model, required=False),
-#                                          label=u'item'
-#                                          ),
-#                                    Field('order',
-#                                          conv=convs.Int(),
-#                                          label=u'order'),
-#                                    ]
-#                                )
-#                            )
-#                        ]
-#                    )
-#                )
-#            ]
-#    return GroupedListItemForm
 
 
 class ListEditAction(StreamAction):
@@ -91,23 +68,25 @@ class ListEditAction(StreamAction):
 
     def __call__(self, env, data):
         list_item_form = self.ListItemForm(env)
+        ordering_field = list_item_form.ordering_field
         if list_item_form is not None:
 
             if list_item_form.accept(env.request.POST):
                 modified_items = []
                 lock_messages = []
                 edit_sessions = {}
-                for item in list_item_form.python_data['items']:
-                    if item['item'].order != item['order']:
+                for value in list_item_form.python_data['items']:
+                    item = value['item']
+                    if getattr(item, ordering_field) != value["order"]:
                         try:
-                            edit_sessions[item['item']] = env.item_lock.create(item['item'])
+                            edit_sessions[item] = env.item_lock.create(item)
                         except ModelLockedByOther, e:
                             lock_messages.append(unicode(e))
                         except ModelLockError, e:
                             lock_messages.append(unicode(e))
 
-                        item['item'].order=item['order']   
-                        modified_items.append(item['item'])
+                        setattr(item, ordering_field, value['order'])
+                        modified_items.append(item)
 
                 if len(lock_messages) > 0:
                     env.db.rollback()
@@ -135,65 +114,3 @@ class ListEditAction(StreamAction):
         return web.method('POST') | web.match('/edit', 'list_edit') | self
 
 
-#class StreamGrouped(BaseStream):
-#    #items must be order by group_by field
-#    default_group = None
-#    group_by = None
-#
-#    def prepare_data(self, request, **filter_params):
-#        data = BaseStream.prepare_data(self, request, **filter_params)
-#        paginator = data['paginator']
-#        data['grouped_items'] = []
-#        prev_group = self.default_group
-#        for item in paginator.items:
-#            if getattr(item, self.group_by) != prev_group:
-#                data['grouped_items'].append([])
-#                prev_group = getattr(item, self.group_by)
-#
-#            data['grouped_items'][-1].append(item)
-#            
-#        data['list_item_form_items'] = data['grouped_items']
-#        return data
-#
-#    def getListItemFormInitial(self, items=[]):
-#        return {'groups': [{'items': [{'item': item, 'order': item.order} for item in group_items ]}
-#                for group_items in items]}
-#
-#    def list_edit(self, request):
-#        list_item_form = self.getListItemForm(request)
-#        if list_item_form is not None:
-#            
-#            if list_item_form.accept(request.form):
-#                modified_items = []
-#                lock_messages = []
-#                edit_sessions = {}
-#
-#                for group in list_item_form.python_data['groups']:
-#                    for item in group['items']:
-#                        if item['item'].order != item['order']:
-#                            try:
-#                                edit_sessions[item['item']] = request.create_item_lock(item['item'])
-#                            except ModelLockedByOther, e:
-#                                lock_messages.append(unicode(e))
-#                            except ModelLockError, e:
-#                                lock_messages.append(unicode(e))
-#
-#                            item['item'].order=item['order']   
-#                            modified_items.append(item['item'])
-#
-#                if len(lock_messages) > 0:
-#                    request.db.rollback()
-#                    request.flash(
-#                        Markup(u'Изменения не были сохранены:<br/>%s') %
-#                        BR.join(lock_messages),
-#                        'failure')
-#                elif len(modified_items) > 0:
-#                    request.db.commit()
-#                    request.flash(BR.join([u'Объект (%s) сохранен' % (item,) for item in modified_items]), 'success')
-#                    [request.remove_item_lock(item, edit_session) for item, edit_session in edit_sessions.items()]
-#
-#            else:
-#                request.flash(
-#                    Markup(u'Изменения не были сохранены из-за ошибок в форме:<br/>%s') % 
-#                    BR.join(list_item_form.errors), 'failure') 
-#        return see_other(self.stream_url(request))
