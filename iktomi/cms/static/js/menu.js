ccddbbc0c4d9b9e691308c6eb8fd4592c7a63c70
Ilya Boltnev <roadtouniverse@gmail.com>
2014-10-29 14:20:40 +0300

    menu items highlighting with filters

--- iktomi/cms/static/js/menu.js
+++ iktomi/cms/static/js/menu.js
@@ -2,15 +2,40 @@
   function menu(elem){
     if(! elem.getParent('.popup')){
       $$('.navigation li.active').removeClass('active');
-      var active = document.getElement('.navigation [data-endpoint="'+elem.dataset.menu+'"]');
-      if (active){
-        if (active.tagName == 'LI'){
-          active.addClass('active');
-        }
-        active.getParents('li').addClass('active');
+      var filter_query = location.search.substr(1).split('&');
+      var menu_items = $$('.navigation [data-endpoint="'+elem.dataset.menu+'"]');
+      menu_items = menu_items.filter(function(element){
+       if(element.tagName == 'A'){
+         var links = [element];
+       }else{
+         var links = [].slice.call(element.getElementsByTagName('A'));
+       }
+       return links.some(function(link){
+          link_filters = (link.get('href').split('?')[1] || '').split('&');
+          return filter_query.some(function(filter){
+            return link_filters.indexOf(filter) >= 0;
+          })
+        });
+      });
+      if(menu_items){
+        menu_items.forEach(function(item){
+          makeActive(item);
+        });
       }
+      else{
+        var active = document.getElement('.navigation [data-endpoint="'+elem.dataset.menu+'"]');
+        makeActive(active);
+      }
+      
       elem.destroy()
     }
+  }
+    
+  function makeActive(item){
+    if (item.tagName == 'LI'){
+        item.addClass('active');
+      }
+    item.getParents('li').addClass('active');
   }
 
   function title(elem){
