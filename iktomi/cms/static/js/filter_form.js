ce5dc147edf85b3e735f0f4f61a6ea677edced6e
Ilya Boltnev <roadtouniverse@gmail.com>
2014-11-20 13:14:31 +0300

    getSize().y => getHeight()

--- iktomi/cms/static/js/filter_form.js
+++ iktomi/cms/static/js/filter_form.js
@@ -12,7 +12,7 @@
     form.getElement('.sidefilter-close').addEvent('click', function(e){
       var sidefilter = this.form.getParent('.sidefilter');
       sidefilter.toggleClass('is-open');
-      var minHeight = sidefilter.getElement('form').getSize().y + 30;
+      var minHeight = sidefilter.getElement('form').getHeight() + 30;
       this.form.getParent('.popup-body').setStyle('min-height', minHeight);
     }.bind(this));
     form.getElement('.sidefilter-tags').addEvent('click', function(e){
