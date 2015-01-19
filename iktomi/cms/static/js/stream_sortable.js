1f83b80c86b52a5b3083efcc89579cc26e652250
Ilya Boltnev <roadtouniverse@gmail.com>
2015-01-19 15:01:51 +0300

    checking browser by name

--- iktomi/cms/static/js/stream_sortable.js
+++ iktomi/cms/static/js/stream_sortable.js
@@ -9,7 +9,7 @@
       this.itemsSelector = el.dataset.selector;
       this.items = this.itemsBlock.getChildren(this.itemsSelector);
       if(this.items.length > 1) {
-        if (navigator.userAgent.match(/AppleWebKit\/\d\d\d/) != null){
+        if (['chrome'].indexOf(Browser.name) >= 0){
           this.boxify();
           window.addEvent('resize', function(){
             this.unboxify();
