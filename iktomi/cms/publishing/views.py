709c9c7acc5bb8b6862d217e52a38f11974e8836
Harut <yes@harutune.name>
2015-09-03 21:18:22 +0300

    publishing queue for i18n views

--- iktomi/cms/publishing/views.py
+++ iktomi/cms/publishing/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from .stream import PublishStream
+from .i18n_stream import I18nPublishStream
 from iktomi.web import WebHandler
 from iktomi.cms.stream_handlers import insure_is_xhr
+from iktomi.utils.storage import VersionedStorage
+
 
 class PublishQueue(WebHandler):
 
@@ -10,19 +13,35 @@ class PublishQueue(WebHandler):
 
     def publish_queue(self, env, data):
         insure_is_xhr(env)
-        # XXX
-        env.models = env.models.admin
-        env.version = 'admin'
+
         changed = []
         for stream in self.streams.values():
-            if isinstance(stream, PublishStream):
-                items = stream.item_query(env)\
+            if isinstance(stream, I18nPublishStream):
+                for lang, lang_name in stream.langs:
+                    subenv = VersionedStorage()
+                    subenv._parent_storage = env
+                    subenv.models = getattr(env.models.admin, lang)
+                    subenv.version = 'admin'
+                    subenv.lang = lang
+
+                    items = stream.item_query(subenv)\
+                                  .filter_by(has_unpublished_changes=True)
+                    if hasattr(stream.config.Model, 'state'):
+                        items = items.filter_by(state=stream.config.Model.PUBLIC)
+                    items = items.all()
+                    changed += [(stream, subenv, item) for item in items]
+            elif isinstance(stream, PublishStream):
+                subenv = VersionedStorage()
+                subenv._parent_storage = env
+                subenv.models = env.models.admin
+                subenv.version = 'admin'
+                items = stream.item_query(subenv)\
                               .filter_by(has_unpublished_changes=True)
                 if hasattr(stream.config.Model, 'state'):
                     items = items.filter_by(state=stream.config.Model.PUBLIC)
                 items = items.all()
-                changed += [(stream, item) for item in items]
-        #changed.sort(key=lambda x: x.date_changed)
+                changed += [(stream, subenv, item) for item in items]
+        changed.sort(key=lambda x: x[2].updated_dt)
         return env.render_to_response('publish_queue', dict(
             changed = changed,
             menu = env.current_location,
